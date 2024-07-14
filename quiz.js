151a08bcaee9151d0a85612e3e520f6b0c144e92
Tanya-abi <tchigwda6@gmail.com>
2024-07-14 17:32:37 +0200

    Quiz

--- quiz.js
+++ quiz.js
@@ -1,20 +1,16 @@
 function checkAnswer(){
     const correctAnswer = "4";
 
-    let userAnswer = document.querySelector('input[name="quiz"]:checked');
+    let userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
      
 
     let submit = document.getElementById('feedback');
 
-  if (userAnswer) {
-    if(userAnswer.value === correctAnswer){
+    if(userAnswer === correctAnswer){
         feedback.textContent = "Correct! Well done.";
     } else {
         feedback.textContent ="That's incorrect. Try again!";
     }
-}else {
-    feedback.textContent = "Please select an answer";
-}
 
 }
 
